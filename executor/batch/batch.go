1aff865ba1fe041eead4ff1971d6778da32e1f39
sh-cha <snghuncha@gmail.com>
2024-10-11 18:37:50 +0900

    change batch file permission

--- executor/batch/batch.go
+++ executor/batch/batch.go
@@ -120,7 +120,7 @@ func (bs *BatchSubmitter) Initialize(ctx context.Context, processedHeight int64,
 	}
 
 	fileFlag := os.O_CREATE | os.O_RDWR | os.O_APPEND
-	bs.batchFile, err = os.OpenFile(bs.homePath+"/batch", fileFlag, 0666)
+	bs.batchFile, err = os.OpenFile(bs.homePath+"/batch", fileFlag, 0640)
 	if err != nil {
 		return err
 	}
