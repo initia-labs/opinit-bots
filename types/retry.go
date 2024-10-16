f6ebcf22d4ad96bd289015cd9db51a573a1c4342
sh-cha <snghuncha@gmail.com>
2024-10-16 18:09:33 +0900

    add timer stop & make retry count as const

--- types/retry.go
+++ types/retry.go
@@ -7,10 +7,12 @@ import (
 	"time"
 )
 
+const MaxRetryCount = 7
+
 func SleepWithRetry(ctx context.Context, retry int) {
 	// to avoid to sleep too long
-	if retry > 7 {
-		retry = 7
+	if retry > MaxRetryCount {
+		retry = MaxRetryCount
 	} else if retry == 0 {
 		return
 	}
@@ -18,6 +20,7 @@ func SleepWithRetry(ctx context.Context, retry int) {
 	sleepTime := 2 * math.Exp2(float64(retry))
 	sleepTime += rand.Float64() * float64(sleepTime) * 0.5 //nolint:all
 	timer := time.NewTimer(time.Duration(sleepTime) * time.Second)
+	defer timer.Stop()
 	select {
 	case <-ctx.Done():
 		return
