28e1067b5d2b0aa7c642f50f20fa2dab936e6f7f
sh-cha <snghuncha@gmail.com>
2024-11-11 17:06:26 +0900

    change cmd name

--- cmd/opinitd/db.go
+++ cmd/opinitd/db.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/initia-labs/opinit-bots/bot"
 	bottypes "github.com/initia-labs/opinit-bots/bot/types"
 	"github.com/initia-labs/opinit-bots/db"
@@ -8,21 +10,29 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// temporary db migration command for v0.1.5
+// migration015Cmd handles the one-time migration of withdrawal data for v0.1.5
+// TODO: Remove this command in the future
 func migration015Cmd(ctx *cmdContext) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "migration",
-		Args:  cobra.ExactArgs(0),
-		Short: "Migration command for v0.1.5",
-		Long: `Migration command for v0.1.5
+		Use:   "migrate",
+		Args:  cobra.ExactArgs(1),
+		Short: "Run database migrations",
+		Long: `Run database migrations
+v0.1.5: Store the sequence number so that it can be accessed by address
 `,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			db, err := db.NewDB(bot.GetDBPath(ctx.homePath, bottypes.BotTypeExecutor))
-			if err != nil {
-				return err
+			version := args[0]
+			switch version {
+			case "v0.1.5":
+				// Run migration for v0.1.5
+				db, err := db.NewDB(bot.GetDBPath(ctx.homePath, bottypes.BotTypeExecutor))
+				if err != nil {
+					return err
+				}
+				return executor.Migration015(db)
+			default:
+				return fmt.Errorf("unknown migration version: %s", version)
 			}
-
-			return executor.Migration015(db)
 		},
 	}
 	return cmd
