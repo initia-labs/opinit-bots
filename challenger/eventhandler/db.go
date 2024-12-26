2e2dc677452fb473ec08f10fa41924e31a920f5c
sh-cha <snghuncha@gmail.com>
2024-12-26 17:26:30 +0900

    challenger eventhandler test

--- challenger/eventhandler/db.go
+++ challenger/eventhandler/db.go
@@ -1,9 +1,35 @@
 package eventhandler
 
 import (
+	"cosmossdk.io/errors"
 	challengertypes "github.com/initia-labs/opinit-bots/challenger/types"
 	"github.com/initia-labs/opinit-bots/types"
 )
+
+func GetPendingEvents(db types.DB, ids []challengertypes.ChallengeId) ([]challengertypes.ChallengeEvent, error) {
+	events := make([]challengertypes.ChallengeEvent, 0)
+	for _, id := range ids {
+		event, err := GetPendingEvent(db, id)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return events, nil
+}
+
+func GetPendingEvent(db types.BasicDB, id challengertypes.ChallengeId) (challengertypes.ChallengeEvent, error) {
+	data, err := db.Get(challengertypes.PrefixedPendingEvent(id))
+	if err != nil {
+		return nil, err
+	}
+
+	event, err := challengertypes.UnmarshalChallengeEvent(id.Type, data)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal challenge event")
+	}
+	return event, err
+}
 
 func SavePendingEvents(db types.BasicDB, events []challengertypes.ChallengeEvent) error {
 	for _, event := range events {
@@ -54,4 +80,24 @@ func DeleteAllPendingEvents(db types.DB) error {
 		}
 	}
 	return nil
+}
+
+func LoadPendingEvents(db types.DB) (events []challengertypes.ChallengeEvent, err error) {
+	iterErr := db.Iterate(challengertypes.PendingEventKey, nil, func(key, value []byte) (stop bool, err error) {
+		id, err := challengertypes.ParsePendingEvent(key)
+		if err != nil {
+			return true, errors.Wrap(err, "failed to parse pending event key")
+		}
+
+		event, err := challengertypes.UnmarshalChallengeEvent(id.Type, value)
+		if err != nil {
+			return true, errors.Wrap(err, "failed to unmarshal challenge event")
+		}
+		events = append(events, event)
+		return false, nil
+	})
+	if iterErr != nil {
+		return nil, iterErr
+	}
+	return
 }
