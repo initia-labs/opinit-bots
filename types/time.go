88636b221dddb679ab1966592934a50f21197b7a
sh-cha <snghuncha@gmail.com>
2025-01-24 13:58:47 +0900

    add mutex to timestamp fetcher

--- types/time.go
+++ types/time.go
@@ -1,11 +1,17 @@
 package types
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 var lastTimestamp int64
+var tsMutex = sync.Mutex{}
 
 // CurrentNanoTimestamp returns the current time in nanoseconds.
 var CurrentNanoTimestamp = func() int64 {
+	tsMutex.Lock()
+	defer tsMutex.Unlock()
 	res := time.Now().UTC().UnixNano()
 	if res == lastTimestamp {
 		res++
