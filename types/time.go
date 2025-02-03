a7b28b62e8119896126926a19cc36d463e2236c4
Hoon <48665813+sh-cha@users.noreply.github.com>
2025-02-03 12:24:01 +0900

    fix conflicting timestamp (#68)
    * fix conflicting timestamp
    
    * add mutex to timestamp fetcher

--- types/time.go
+++ types/time.go
@@ -1,8 +1,22 @@
 package types
 
-import "time"
+import (
+	"sync"
+	"time"
+)
+
+var lastTimestamp int64
+var tsMutex = sync.Mutex{}
 
 // CurrentNanoTimestamp returns the current time in nanoseconds.
 var CurrentNanoTimestamp = func() int64 {
-	return time.Now().UTC().UnixNano()
+	tsMutex.Lock()
+	defer tsMutex.Unlock()
+	res := time.Now().UTC().UnixNano()
+	if res == lastTimestamp {
+		res++
+	}
+
+	lastTimestamp = res
+	return res
 }
